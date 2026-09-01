1aa9e7939c32de116902570b22f89458b20d803e
bself-rgb <bself@secondleash.us>
2026-09-01 00:26:57 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Second Leash Hub configuration
 // This is the public/publishable Supabase key. Never put a service_role/secret key here.
 const SUPABASE_URL = "https://qkftfbatpijxrhaeaymj.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrZnRmYmF0cGlqeHJoYWV5bWoiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc4NzM2MDc2MSwiZXhwIjoyMTAyOTM2NzYwfQ.VI2TvdRXlRxloRjdIy7olMxrQt7JvWE2tmu6LujEkUM";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_ig8sZiYigMY4ANhtlOFFKA_HGNPsP-u";
